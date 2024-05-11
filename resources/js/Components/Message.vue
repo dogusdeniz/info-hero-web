e53f0d857b69f62332f4dda04c336e09c30103a1
dogusdeniz <dogus0deniz@gmail.com>
2024-05-11 11:13:00 +0300

    chore: Update chat component with new branding and add Message.vue component

---
+++ resources/js/Components/Message.vue
@@ -0,0 +1,59 @@
+<script setup>
+defineProps({
+    name: String,
+    message: String,
+    avatar: String,
+    messageId: String,
+    messageAuthorRole: String,
+});
+</script>
+<template>
+    <div>
+        <div class="py-2 px-3 text-base md:px-4 m-auto lg:px-1 xl:px-5">
+            <div
+                class="mx-auto flex flex-1 gap-3 text-base juice:gap-4 juice:md:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"
+            >
+                <div class="flex-shrink-0 flex flex-col relative items-end">
+                    <div>
+                        <div class="pt-0.5">
+                            <div
+                                class="hermit-shadow-stroke flex h-6 w-6 items-center justify-center overflow-hidden rounded-full"
+                            >
+                                <div class="relative flex">
+                                    <img
+                                        alt="User"
+                                        referrerpolicy="no-referrer"
+                                        loading="lazy"
+                                        width="24"
+                                        height="24"
+                                        decoding="async"
+                                        class="rounded-sm"
+                                        :src="avatar"
+                                        style="color: transparent"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="relative flex w-full min-w-0 flex-col">
+                    <div class="font-semibold select-none">{{ name }}</div>
+                    <div class="flex-col gap-1 md:gap-3">
+                        <div class="flex flex-grow flex-col max-w-full">
+                            <div
+                                :data-message-author-role="messageAuthorRole"
+                                :data-message-id="messageId"
+                                dir="auto"
+                                class="min-h-[20px] text-message flex flex-col items-start whitespace-pre-wrap break-words overflow-x-auto gap-3"
+                            >
+                                <div class="relative max-w-[90%]">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
