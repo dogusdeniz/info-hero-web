194b7886927141a1e796abe6904093caeb067c42
dogusdeniz <dogus0deniz@gmail.com>
2024-05-12 12:02:47 +0300

    feat: Add migration for creating documents table

---
+++ database/migrations/2024_05_11_125446_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+
+            $table->uuid();
+            $table->string('name');
+            $table->string('path');
+            $table->string('ext');
+            $table->string('mime');
+            $table->string('size');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
